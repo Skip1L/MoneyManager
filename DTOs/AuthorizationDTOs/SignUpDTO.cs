60f4fe57754497233c2fc6054760d0c4ea89afb6
Skip1L <96205978+Skip1L@users.noreply.github.com>
2025-01-07 19:26:48 +0200

    Update sign up dto

--- DTOs/AuthorizationDTOs/SignUpDTO.cs
+++ DTOs/AuthorizationDTOs/SignUpDTO.cs
@@ -4,10 +4,9 @@ namespace DTOs.AuthorizationDTOs
 {
     public class SignUpDTO
     {
-        public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string ConfirmPassword { get; set; }
+        public List<string> Roles { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
