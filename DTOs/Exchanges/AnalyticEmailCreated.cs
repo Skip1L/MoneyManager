8f03f313604b56d04ce2355cbbfb23f60b858f14
Skip1L <96205978+Skip1L@users.noreply.github.com>
2025-02-08 18:22:42 +0200

    add rabbitmq producer

---
+++ DTOs/Exchanges/AnalyticEmailCreated.cs
@@ -0,0 +1,18 @@
+﻿using DTOs.CommonDTOs;
+using DTOs.NotidicationDTOs;
+using DTOs.TransactionDTOs;
+
+namespace DTOs.Exchanges
+{
+    public interface AnalyticEmailCreated
+    {
+        public Guid Id { get; set; }
+        public string RecipientName { get; set; }
+        public string ToEmail { get; set; }
+        public List<CategoryReportDTO> Incomes { get; set; }
+        public List<CategoryReportDTO> Expenses { get; set; }
+        public List<BudgetReportDTO> Budgets { get; set; }
+        public TransactionsSummaryDTO TransactionsSummary { get; set; }
+        public DateRangeFilter DateRange { get; set; }
+    }
+}
---
+++ Services/Jobs/RabbitMqWeeklyAnalyticJob.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using DTOs.Exchanges;
+using DTOs.NotidicationDTOs;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Services.Jobs
+{
+    public class RabbitMqWeeklyAnalyticJob(ILogger<HttpWeeklyAnalyticJob> logger,
+                                           IServiceScopeFactory serviceScopeFactory,
+                                           IPublishEndpoint publishEndpoint,
+                                           IMapper mapper) : BaseWeeklyAnalyticJob(logger, serviceScopeFactory, mapper)
+    {
+        private readonly IPublishEndpoint _publishEndpoint = publishEndpoint;
+        protected override async Task SendReportAsync(AnalyticEmailRequestDTO emailReport, CancellationToken cancellationToken)
+        {
+            await _publishEndpoint.Publish<AnalyticEmailCreated>(new
+            {
+                Id = new Guid(),
+                Budgets = emailReport.Budgets,
+                DateRange = emailReport.DateRange,
+                Expenses = emailReport.Expenses,
+                Incomes = emailReport.Incomes,
+                RecipientName = emailReport.RecipientName,
+                ToEmail = emailReport.ToEmail,
+                TransactionsSummary = emailReport.TransactionsSummary
+            }, cancellationToken);
+
+            logger.LogInformation("Sent Report");
+        }
+    }
+}
